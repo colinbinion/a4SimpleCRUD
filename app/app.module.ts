ec7398de036a5600fcaae3407c0f4105adf9b6cf
colinbinion <colin.binion@me.com>
2017-06-05 10:12:53 -0600

    setting file structure

---
+++ app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { ProductComponent } from './products/product.component';
+import { NavbarComponent } from './nav/nav.component';
+
+@NgModule({
+  imports: [ BrowserModule,
+                   FormsModule,
+                   AppRoutingModule
+                   ],
+  declarations: [ AppComponent,
+                          ProductComponent,
+                          NavbarComponent
+                          ],
+  providers: [ ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
